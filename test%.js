49a6ec2c3a0a3068bb54510f54828d7f995ac17c
Namrata Tanwani <54472042+namratanwani@users.noreply.github.com>
2024-04-23 15:10:45 +0100

    Add files via upload

---
+++ test%.js
@@ -0,0 +1,64 @@
+function fibonacci(num) {
+    if (num <= 1) return num;
+    return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+function isPrime(num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 === 0 || num % 3 === 0) return false;
+    let i = 5;
+    while (i * i <= num) {
+        if (num % i === 0 || num % (i + 2) === 0) return false;
+        i += 6;
+    }
+    return true;
+}
+
+function factorial(num) {
+    if (num === 0) return 1;
+    return num * factorial(num - 1);
+}
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+
+function mergeArrays(arr1, arr2) {
+    return [...arr1, ...arr2];
+}
+
+function bubbleSort(arr) {
+    const len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+class Animal {
+    constructor(name, species) {
+        this.name = name;
+        this.species = species;
+    }
+    makeSound() {
+        console.log("Animal sound");
+    }
+}
+
+const myAnimal = new Animal("Leo", "Lion");
+
+console.log(fibonacci(10));
+console.log(isPrime(17));
+console.log(factorial(5));
+console.log(reverseString("hello"));
+console.log(mergeArrays([1, 2, 3], [4, 5, 6]));
+console.log(bubbleSort([5, 3, 8, 1, 2]));
+console.log(myAnimal);
+myAnimal.makeSound();
